6925672e5aa5a16051ff6866f7155627e0c8147a
Eliana Arias <ariaselianamariel@gmail.com>
2024-10-15 18:58:02 -0300

    Terminé Clase04

--- clase-04/src/main/java/com/coderhouse/Clase04.java
+++ clase-04/src/main/java/com/coderhouse/Clase04.java
@@ -1,9 +1,101 @@
 package com.coderhouse;
 
+import com.coderhouse.objetos.Perro;
+
 public class Clase04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		  
+		Dejo comentado esto solo para hacer lo de abajo con listas. pero anda todo ok
+		
+		Ahora debo instanciar el objeto perro: hora creo el objeto perro
+		Perro  primerPerro = new Perro();//perro es mi molde, y quiero darle un nombre
+
+		Ahora cómo lo nombro? tengo que poder ver los atributos
+		primerPerro.nombre = "Firulais";
+		primerPerro.color = "Marron";
+		primerPerro.edad = '3';
+		primerPerro.raza = "Caniche";
+		primerPerro.tamanio = "Chico";
+				
+		//defino variables
+		
+		primerPerro.setNombre("Firulais");
+		primerPerro.setColor("Marron");
+
+		primerPerro.ladrar();
+		primerPerro.caminar();
+		*/
+		
+		
+		
+		/*Primero hago la lista para usar for each
+		String [] nombres = new String [] {
+			"Lorena",
+			"Alejandro",
+			"Tristan",
+			"Valentin"
+		};
+		
+		ahora como hago para que mi metodo funcione? debo invocarlo
+		
+		imprimirListaDeNombres (nombres);
+		sumarNumeros (5, 66);
+		*/
+		
+		
+		
+		//uso try catch para manejo  del error que sale al ingresar 100 años
+			
+		try {
+		Perro  primerPerro = new Perro();//perro es mi molde, y quiero darle un nombre
+		primerPerro.setNombre ("Firulais");
+		primerPerro.setColor ("Marron");
+		primerPerro.setRaza ("Caniche");
+		primerPerro.setTamanio ("Chico");
+		primerPerro.setEdad (15); //pero no le puedo poner tanto. va a dar error. debo limitarlo. para eso voy a metodo set.edad
+		
+		System.out.println("Perro: " + primerPerro);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/*
+	private static void imprimirListaDeNombres (String [] nombresPersonas) {
+		for (String nombre : nombresPersonas) {
+			System.out.println("Nombre: " + nombre);
+		}
 		
 	}
+	
+	//probamos otro metodo
+	private static void sumarNumeros (int numeroA, int numeroB) {
+		System.out.println("La suma entre "+ numeroA + " y " + numeroB + " es de: " + (numeroA + numeroB)); /*y uso parentesis al final en la suma para que lo sume primero antes de meterlo en la operacion. sin el parentesis estaría concatenando*/
+	};*/
+	//COMENTO LO ANTERIOR PAR Q NO MOLESTE
+	double radio = 4d;
+	
+	System.out.println("El area del circulo con radio " + radio + " es igual a: " 
+	+ calcularAreaDeUnCirculo (radio))
+	
+	
+	//formaa de inicializar una constante estatica
+		//inicializador estatico
+		
+		static final double PI; //puedo inicializar varias constantes aca adentro
+		static final double PO; 
 
+		static {
+			PI = 3.14141414;
+			PO = 30414141; //y puedo poner todo dentro de un mismo renglon desde static si quisiera
+		}
+		private static double calcularAreaDeUnCirculo (double radio) {//calculo el area de un circulo
+			return PI * Math.pow(radio, 2);
+		}
+
+	
+	
+	
 }
--- clase-04/src/main/java/com/coderhouse/objetos/Perro.java
+++ clase-04/src/main/java/com/coderhouse/objetos/Perro.java
@@ -1,5 +1,78 @@
 package com.coderhouse.objetos;
 
 public class Perro {
+	
+	
+	/*si quiero que la edad del perro vaya variando pero dentro de un rango*/
+private static final int EDAD_MAXIMA = 20;//todas las constantes van en mayuscula a dif de las variables y deben inicializarse si o si
+
+
+	/*Defino atributos o variables de clase*/
+	private String nombre;
+	private String raza;
+	private String color;
+	private String tamanio;
+	private int edad;
+
+	String mensajeDeError = "El perro no vive más de " + EDAD_MAXIMA + " años de edad";/*declaro esta variable fuera de los metodos del final, para no tener que repetirlo dentro de cada uno. poniendolo aca le doy acceso global para poder acceder a el desde cualquier parte del codigo*/
+
+		
+	/*Métodos*/
+	public void ladrar () {
+		System.out.println( getNombre ()  + " está ladrando"); /*uso get y no nombre solo para que directamente me tire el nombre*/
+	}
+	public void caminar () {
+		System.out.println( nombre  + " está caminando");
+	}
+	public String getNombre() { /*si quiero traer el valor nombre uso get*/
+		return this.nombre;
+	}
+	public void setNombre(String nombre) { /*para asignarle un valor, inicializarlo*/
+		this.nombre = nombre;
+	}
+	public String getRaza() {
+		return this.raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getColor() {
+		return this.color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTamanio() {
+		return this.tamanio;
+	}
+	public void setTamanio(String tamanio) {
+		this.tamanio = tamanio;
+	}
+	public int getEdad() {
+		return this.edad;
+	}
+	
+	public void setEdad(int edad) throws Exception { //le pide que arroje una excepcion si supera los 20
+		if (edad > EDAD_MAXIMA) {
+			throw new Exception (mensajeDeError);
+		} else {
+		this.edad = edad;
+	}
+}
+	//metodo para imprimir datos: tostring
+	@Override
+	public String toString() {
+		return "Perro [nombre=" + nombre + ", raza=" + raza + ", color=" + color + ", tamanio=" + tamanio + ", edad= " 
+				+ edad + "]" + "\n" + mensajeDeError;
+	}
+	
+	
+	
+	
+	
+	
+	//GETTERS Y SETTERS para recuperar el atributo o setearlo (cambiarlo o inicializarlo)
+	//cuando creo el objeto, con el setter le puedo cambiar el nombre
+	
 
 }
