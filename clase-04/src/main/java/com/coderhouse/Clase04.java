95831c13a05634971637f79275f955fc719a2801
Eliana Arias <ariaselianamariel@gmail.com>
2024-10-20 02:36:17 -0300

    Terminé clase04

--- clase-04/src/main/java/com/coderhouse/Clase04.java
+++ clase-04/src/main/java/com/coderhouse/Clase04.java
@@ -6,96 +6,106 @@ public class Clase04 {
 
 	public static void main(String[] args) throws Exception {
 		
-		/*
+//TODO LO QUE ESTÁ COMENTADO Y TIENE LLAVES, OJO! PORQUE PUEDEN ESTAR REPETIDAS AFUERA PARA QUE FUNCIONE EL CÓDIGO		
 		  
-		Dejo comentado esto solo para hacer lo de abajo con listas. pero anda todo ok
-		
-		Ahora debo instanciar el objeto perro: hora creo el objeto perro
-		Perro  primerPerro = new Perro();//perro es mi molde, y quiero darle un nombre
 
-		Ahora cómo lo nombro? tengo que poder ver los atributos
-		primerPerro.nombre = "Firulais";
-		primerPerro.color = "Marron";
-		primerPerro.edad = '3';
-		primerPerro.raza = "Caniche";
-		primerPerro.tamanio = "Chico";
-				
-		//defino variables
+/*--------------------------------------------------------------------------------------------------
+		 Dejo comentado esto solo para hacer lo de abajo con listas. pero anda todo ok
+		 
 		
-		primerPerro.setNombre("Firulais");
-		primerPerro.setColor("Marron");
+//		//Ahora debo instanciar el objeto perro: hora creo el objeto perro
+//		Perro  primerPerro = new Perro();//perro es mi molde, y quiero darle un nombre
+//
+//		//Ahora cómo lo nombro? tengo que poder ver los atributos
+//		primerPerro.nombre = "Firulais";
+//		primerPerro.color = "Marron";
+//		primerPerro.edad = '3';
+//		primerPerro.raza = "Caniche";
+//		primerPerro.tamanio = "Chico";
+//		
+//		primerPerro.ladrar();
+//		primerPerro.caminar();
+//		
+//		//Defino nombre de mi variable
+//	
+//		primerPerro.setNombre ("Firulais");
+//		primerPerro.setColor("Marron");
+/*------------------------------------------------------------------------------------------------------------*/
 
-		primerPerro.ladrar();
-		primerPerro.caminar();
-		*/
+		
+/*------------------------------------------------------------------------------------------------------------
+					Dejo comentado esto para trabajar excecpciones pero anda todo ok
+		
+//ARRANCO EJEMPLO LISTA: quiero armar una lista y un metodo para recorrerla y ver los nombres
+//		
+//		//Primero hago la lista
+//		String [] nombres = new String [] {
+//			"Lorena",
+//			"Alejandro",
+//			"Tristan",
+//			"Valentin"
+//		};
+//		//y tercero lo invoco para que mi metodo imprimirListaDeNombres funcione
+//		imprimirListaDeNombres(nombres);
+//		//y 2do invoco mi segundo método sumarNumeros
+//		sumarNumeros (5,66);
+//		
+//	}		
+//		//segundo creo mi metodo y la recorro usando for each
+//		private static void imprimirListaDeNombres (String[] nombres) {
+//			for (String nombre : nombres) {
+//			System.out.println("Nombre: " + nombre);	
+//			}
+//		}
+//		
+//		
+//		
+////OTRO MÉTODO: sumarNumeros
+//		//1ro creo mi metodo y sigo arriba
+//		private static void sumarNumeros (int numeroA, int numeroB) {
+//			System.out.println("La suma entre "+ numeroA + " y " + numeroB + " es de: " + (numeroA + numeroB)); /*y uso parentesis al final en la suma para que lo sume primero antes de meterlo en la operacion. sin el parentesis estaría concatenando*/
+//		};										//puede quedar así o puedo pedirle que muestre un resultado
+/*----------------------------------------------------------------------------------------------------------*/
 		
 		
 		
-		/*Primero hago la lista para usar for each
-		String [] nombres = new String [] {
-			"Lorena",
-			"Alejandro",
-			"Tristan",
-			"Valentin"
-		};
+/*---------------------------------------AHORA VAMOS CON EXCEPCIONES----------------------------------------*/
+//traigo de vuelta el ejemplo del perro. voy a trabajar la edad con rango, así controlo el mensaje de error
+		Perro  primerPerro = new Perro();
 		
-		ahora como hago para que mi metodo funcione? debo invocarlo
-		
-		imprimirListaDeNombres (nombres);
-		sumarNumeros (5, 66);
-		*/
-		
-		
-		
-		//uso try catch para manejo  del error que sale al ingresar 100 años
-			
-		try {
-		Perro  primerPerro = new Perro();//perro es mi molde, y quiero darle un nombre
+		try { //trata de imprimir esto
 		primerPerro.setNombre ("Firulais");
-		primerPerro.setColor ("Marron");
-		primerPerro.setRaza ("Caniche");
-		primerPerro.setTamanio ("Chico");
-		primerPerro.setEdad (15); //pero no le puedo poner tanto. va a dar error. debo limitarlo. para eso voy a metodo set.edad
-		
-		System.out.println("Perro: " + primerPerro);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		primerPerro.setColor("Marron");
+		primerPerro.setRaza("caniche");
+		primerPerro.setTamanio("chico");
+		primerPerro.setEdad(10);//acá no puedo poner cualquier num, para controlar eso debo ir a Perro y trabajar algo 1ro
+	//acá no debo olvidar add throws declaration. sino no anda todo lo que hice en Perro. Le va a agregar throws exc al metodo que etá haciendo la instancia (al lado de args arriba de todo)
+		//System.out.println("Perro: " + primerPerro);//funciona si genero tostring en Perro primero
+	} catch (Exception e) { //sino lanzame la excepcion. y la excepcion me pide un mensaje de error
+		e.printStackTrace(); //esto es asi. no tocamos nada mas
 	}
-	/*
-	private static void imprimirListaDeNombres (String [] nombresPersonas) {
-		for (String nombre : nombresPersonas) {
-			System.out.println("Nombre: " + nombre);
-		}
 		
-	}
-	
-	//probamos otro metodo
-	private static void sumarNumeros (int numeroA, int numeroB) {
-		System.out.println("La suma entre "+ numeroA + " y " + numeroB + " es de: " + (numeroA + numeroB)); /*y uso parentesis al final en la suma para que lo sume primero antes de meterlo en la operacion. sin el parentesis estaría concatenando*/
-	};*/
-	//COMENTO LO ANTERIOR PAR Q NO MOLESTE
-	double radio = 4d;
-	
-	System.out.println("El area del circulo con radio " + radio + " es igual a: " 
-	+ calcularAreaDeUnCirculo (radio))
-	
-	
-	//formaa de inicializar una constante estatica
-		//inicializador estatico
 		
-		static final double PI; //puedo inicializar varias constantes aca adentro
+/*------------------------------INICIALIZADOR ESTÁTICO DENTRO DE MAIN-------------------------------------*/
+	
+		
+		double radio = 4d; //3ro para eso declaro esta variable, para usar un random en mi msj
+
+		System.out.println("El area del circulo con radio\n " + radio + " es igual a: " //2do compruebo q la variable anda imprimiendola
+		+ calcularAreaDeUnCirculo (radio));
+}		//esta llave va aca para que el static quede fuera del main
+	
+	//1ro inicializo mi constante estatica: con un inicializador estatico
+		static final double PI; //esto va fuera del main pero el metodo lo voy a hacer dentro, es decir arriba
 		static final double PO; 
 
-		static {
+		static {//puedo inicializar varias constantes aca adentro
 			PI = 3.14141414;
 			PO = 30414141; //y puedo poner todo dentro de un mismo renglon desde static si quisiera
 		}
 		private static double calcularAreaDeUnCirculo (double radio) {//calculo el area de un circulo
-			return PI * Math.pow(radio, 2);
+			return PI * Math.pow(radio, 2); //calculame pi*radio al cuadrado. que haga potencia entre radio y cuadrado
 		}
-
-	
-	
-	
+		
 }
+
--- clase-04/src/main/java/com/coderhouse/objetos/Perro.java
+++ clase-04/src/main/java/com/coderhouse/objetos/Perro.java
@@ -3,32 +3,35 @@ package com.coderhouse.objetos;
 public class Perro {
 	
 	
-	/*si quiero que la edad del perro vaya variando pero dentro de un rango*/
-private static final int EDAD_MAXIMA = 20;//todas las constantes van en mayuscula a dif de las variables y deben inicializarse si o si
+	/*EXCEPCIONES: si quiero evitar un error en edad, espero que la edad del perro vaya variando pero dentro de un rango*/
+	private static final int EDAD_MAXIMA = 15;//todas las constantes van en mayusc a dif de las variables y deben inicializarse si o sim es decir hay q ponerle =20
 
 
 	/*Defino atributos o variables de clase*/
-	private String nombre;
-	private String raza;
-	private String color;
-	private String tamanio;
-	private int edad;
+	public String nombre;
+	public String raza;
+	public String color;
+	public String tamanio;// tamanio sin ñ porque en teclado ingles no existe ni tildes
+	public int edad; // int para que la edad no sea 1.4 o 0.06
 
-	String mensajeDeError = "El perro no vive más de " + EDAD_MAXIMA + " años de edad";/*declaro esta variable fuera de los metodos del final, para no tener que repetirlo dentro de cada uno. poniendolo aca le doy acceso global para poder acceder a el desde cualquier parte del codigo*/
+	String mensajeDeError = "El perro no vive más de " + EDAD_MAXIMA + " años de edad";/*declaro esta variable fuera del metodo exception del final, para no tener que repetirlo dentro de cada uno. poniendolo aca le doy acceso global para poder acceder a el desde cualquier parte del codigo*/
 
 		
 	/*Métodos*/
 	public void ladrar () {
-		System.out.println( getNombre ()  + " está ladrando"); /*uso get y no nombre solo para que directamente me tire el nombre*/
+		System.out.println(getNombre() + " está ladrando"); /*uso get y no nombre solo para que directamente me tire el nombre*/
 	}
+	
 	public void caminar () {
-		System.out.println( nombre  + " está caminando");
+		System.out.println(getNombre()  + " está caminando");
 	}
-	public String getNombre() { /*si quiero traer el valor nombre uso get*/
+	
+	/*Getters and setters*/
+	public String getNombre() {
 		return this.nombre;
 	}
-	public void setNombre(String nombre) { /*para asignarle un valor, inicializarlo*/
-		this.nombre = nombre;
+	public String setNombre(String nombre) {
+		return this.nombre;
 	}
 	public String getRaza() {
 		return this.raza;
@@ -52,27 +55,33 @@ private static final int EDAD_MAXIMA = 20;//todas las constantes van en mayuscul
 		return this.edad;
 	}
 	
-	public void setEdad(int edad) throws Exception { //le pide que arroje una excepcion si supera los 20
-		if (edad > EDAD_MAXIMA) {
+	//para manejar el problema de la edad seteo un mje de error (excepcion)
+	public void setEdad(int edad) throws Exception {
+		//y antes de asignarle la edad debo validar si se puede o no
+		//eso lo chequeo con if. pero antes de if, vamos a arrojar una excepción. y eso lo hago poniendo throwException despues de (int edad)para que arroje un msj de error
+		if (edad >=EDAD_MAXIMA) {//chequea el rango  -------> ahora quiero convertir 20 en una variable que pueda ir cambiando. lo hago arriba abajo de main
 			throw new Exception (mensajeDeError);
-		} else {
-		this.edad = edad;
+		} else {//y sino me va a permitir hacer la asignacion
+			this.edad = edad; 
+		}
 	}
-}
-	//metodo para imprimir datos: tostring
+	
+	 //ahora cómo imprimo la edad? to string: source - generate to string
 	@Override
 	public String toString() {
-		return "Perro [nombre=" + nombre + ", raza=" + raza + ", color=" + color + ", tamanio=" + tamanio + ", edad= " 
-				+ edad + "]" + "\n" + mensajeDeError;
+		return "Perro [nombre=" + nombre + ", raza=" + raza + ", color=" + color + ", tamanio=" + tamanio + ", edad="
+				+ edad + "]";
 	}
 	
 	
 	
+}
 	
+
 	
 	
 	//GETTERS Y SETTERS para recuperar el atributo o setearlo (cambiarlo o inicializarlo)
 	//cuando creo el objeto, con el setter le puedo cambiar el nombre
 	
 
-}
+
