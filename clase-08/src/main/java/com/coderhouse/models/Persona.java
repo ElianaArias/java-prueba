979d3a9990b74d5373e9fd9c26272498e892e3a2
Eliana Arias <ariaselianamariel@gmail.com>
2024-10-23 22:41:53 -0300

    Creo clase08

---
+++ clase-08/src/main/java/com/coderhouse/models/Persona.java
@@ -0,0 +1,59 @@
+package com.coderhouse.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Persona {
+
+	@Id //con esto digo que es primary key. debe ser incremental y no nulo
+	@GeneratedValue (strategy = GenerationType.IDENTITY) //esto lo hago para que el id sea autoincremental
+	private Long id;
+	
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	//ahora le digo cómo se van a relacionar y creo la col pasaporte
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pasaporte_id", referencedColumnName = "id")  //aca le digo con qué columna hacer join
+	private Pasaporte pasaporte;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Pasaporte getPasaporte() {
+		return pasaporte;
+	}
+
+	public void setPasaporte(Pasaporte pasaporte) {
+		this.pasaporte = pasaporte;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", pasaporte=" + pasaporte + "]";
+	}
+	
+	
+	
+}
